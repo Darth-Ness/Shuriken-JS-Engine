99e596a83cdb1c61644e61c8197211948d23bb3d
Darth-Ness <75047141+Darth-Ness@users.noreply.github.com>
2021-12-05 20:35:47 +0000

    Add support for importing.

--- src/parserJS.py
+++ src/parserJS.py
@@ -1,7 +1,9 @@
-fileName = input("Please select a JavaScript file: ")
-#fileName = "test.js"
-with open(fileName, "r") as file:JS=file.read().split("\n")
-i = 0
-while(i < len(JS)):
-    if ("document.write" in JS[i]): print(JS[i][JS[i].index("\"")+1:len(JS[i])-3])
-    i+=1
+def parse():
+    fileName = input("Please select a JavaScript file: ")
+    #fileName = "test.js"
+    with open(fileName, "r") as file:JS=file.read().split("\n")
+    i = 0
+    while(i < len(JS)):
+        if ("document.write" in JS[i]): print(JS[i][JS[i].index("\"")+1:len(JS[i])-3])
+        i+=1
+parse()
