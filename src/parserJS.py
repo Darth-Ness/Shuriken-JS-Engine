dedf5b38df5b756627ab74814b76f7f3c20de1a8
Darth-Ness <75047141+Darth-Ness@users.noreply.github.com>
2021-12-05 21:48:34 +0000

    Update Shuriken to be compatable with Katana.

--- src/parserJS.py
+++ src/parserJS.py
@@ -1,9 +1,12 @@
-def parse():
-    fileName = input("Please select a JavaScript file: ")
-    #fileName = "test.js"
-    with open(fileName, "r") as file:JS=file.read().split("\n")
-    i = 0
+def parseOutJS(toRender):
+    start,end,result,i = toRender.index("<script>")+1, toRender.index("</script>")-1, [],0
+    if start != end: 
+        while(i < end):result.append(toRender[i])
+    else:result.append(toRender[start])
+    return result
+def executeJS(root, toRender,C):
+    i,JS = 0,parseOutJS(toRender)
+    from tkinter import Label,TOP
     while(i < len(JS)):
-        if ("document.write" in JS[i]): print(JS[i][JS[i].index("\"")+1:len(JS[i])-3])
+        if ("document.write" in JS[i]):Label(root, text=(JS[i][JS[i].index("\"")+1:len(JS[i])-3]), fg="black", height= 1, borderwidth=0, bg=root.cget('bg'), font= ("Helvetica", 16)).pack(side=TOP, anchor=C)
         i+=1
-parse()
